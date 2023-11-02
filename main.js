d8809baaf513b5bf9ce255ed9cc58258e0f4ff4f
minhkhang45 <famfiona@yandex.ru>
2023-11-02 18:46:35 +0500

    added burger button animation

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 let burgnav = document.querySelector(".burg-nav")
 let burger = document.querySelector(".burger")
+let one = document.querySelector(".ones")
+let two = document.querySelector(".twos")
+let three = document.querySelector(".threes")
 
 burger.addEventListener("click", myFunction);
 
 function myFunction() {
-    if (burgnav.style.transform == "translateX(0%)") {
-        burgnav.style.transform = "translateX(100%)"
-      } else {
-        burgnav.style.transform = "translateX(0%)"
-      }
+  if (burgnav.style.transform == "translateX(0%)") {
+    burgnav.style.transform = "translateX(100%)"
+  } else {
+    burgnav.style.transform = "translateX(0%)"
+  }
+  one.classList.toggle("click")
+  two.classList.toggle("click")
+  three.classList.toggle("click")
 }
